78bfaa6a1287da7d7fa434e3d93da0e46993dafc
Mohamed Amine BEN RAYANA <medamine.benrayana@gmail.com>
2021-07-30 00:37:29 +0200

    implement printAllSubsets function

--- src/main/java/com/infine/kata/Ex04.java
+++ src/main/java/com/infine/kata/Ex04.java
@@ -7,7 +7,18 @@ import java.util.Set;
 public class Ex04 {
     static Set<String> printAllSubsets(List<Integer> list) {
         Set<String> result = new HashSet<>();
-
+        result.add("");
+        combination("", list, 0, result);
+        
         return result;
+    }
+    static void combination(String s, List<Integer> list, int index, Set<String> result)
+    {
+    	for(int i = index; i < list.size(); i++)
+    	{
+    		String newCombi = s+ String.valueOf(list.get(i))+",";
+    		result.add(newCombi);
+    		combination(newCombi, list, i+1, result);
+    	}
     }
 }
