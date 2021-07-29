1fd6337c968129ecccce20e37f0ec92b49ef9687
Mohamed Amine BEN RAYANA <medamine.benrayana@gmail.com>
2021-07-29 22:38:06 +0200

    correct Ex02 class

--- src/main/java/com/infine/kata/Ex02.java
+++ src/main/java/com/infine/kata/Ex02.java
@@ -1,6 +1,8 @@
 package com.infine.kata;
 
-public class Ex02  {
+import java.io.Serializable;
+
+public class Ex02  implements Serializable{
     private String name;
     private double value;
 
@@ -9,5 +11,35 @@ public class Ex02  {
         this.value = value;
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(value);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
 
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ex02 other = (Ex02) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+			return false;
+		return true;
+	}
+
+    
 }
