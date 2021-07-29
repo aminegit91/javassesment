1d6bfb2c98098484753fec365477ea30ed947d29
Mohamed Amine BEN RAYANA <medamine.benrayana@gmail.com>
2021-07-29 23:50:57 +0200

    implement countStationsGroupByLineId function

--- src/main/java/com/infine/kata/Ex01.java
+++ src/main/java/com/infine/kata/Ex01.java
@@ -5,14 +5,26 @@ import java.util.Map;
 
 public class Ex01 {
 
-    /*
-    implementer plusieurs version de la méthode 
-    qui retourne une map contenant le nombre de stations par ligne
-    v1) compatible java 7
-    v2) en utilisant les lambda
-    v3) faire une version parallèle
-    */
-    Map<String, Long> countStationsGroupByLineId() {
-         return new HashMap<>();
-    }
+	/*
+	 * implementer plusieurs version de la méthode qui retourne une map contenant le
+	 * nombre de stations par ligne v1) compatible java 7 v2) en utilisant les
+	 * lambda v3) faire une version parallèle
+	 */
+	Map<String, Long> countStationsGroupByLineId() {
+
+		Trafic[] tab = Trafic.trafic;
+
+		Map<String, Long> result = new HashMap<String, Long>();
+
+		for (int i = 0; i < tab.length; i++) {
+			String ligne = tab[i].getLine();
+			if (result.containsKey(ligne)) {
+				result.put(ligne, result.get(ligne) + 1);
+			} else {
+				result.put(ligne, 1L);
+			}
+		}
+
+		return result;
+	}
 }
